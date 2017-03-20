077139a80bd88604942a59b20aeba7e413becba6
Mark Wong <markmcwong@yahoo.com>
2017-03-20 05:01:45 +0000

    revert back to wunderlist with json

--- src/App.js
+++ src/App.js
@@ -18,28 +18,35 @@ class App extends Component {
 	    }
 	}
     test = () =>{
-		window.location.href = 'https://todoist.com/oauth/authorize?client_id=e37d7bbe9c4b4162a4f9bb738d7fcad9&scope=data:read,data:delete&state=secretstring';
+		window.location.href = 'https://www.wunderlist.com/oauth/authorize?client_id=518dd73af2d377583d18&redirect_uri=https://react-markmcwong.c9users.io/oauth2/callback&state=RANDOM';
     }
     a = () => {
 		var form = new FormData();
-		form.append("client_id", "e37d7bbe9c4b4162a4f9bb738d7fcad9");
-		form.append("client_secret", "c25d84a7a2af4992a9b11217b2eae205");
+		form.append("client_id", "518dd73af2d377583d18");
+		form.append("client_secret", "51a19ee8540b5eb840d4ddb4870bcb3a03cc49ac3754aa69800f64299a26");
 		form.append("code", this.apiUrl);
-		form.append("redirect_uri", "http://react-markmcwong.c9users.io/");
-		
+		//form.append("redirect_uri", "http://react-markmcwong.c9users.io/");
+		for (var pair of form.entries()) {
+    		console.log(pair[0]+ ', ' + pair[1]); 
+		}
 		axios({
 		  "async": true,
 		  "crossDomain": true,
-		  "url": "https://thingproxy.freeboard.io/fetch/https://todoist.com/oauth/access_token",
+		  "url": "https://thingproxy.freeboard.io/fetch/https://www.wunderlist.com/oauth/access_token",
 		  "method": "POST",
 		  "headers": {
 		    "cache-control": "no-cache",
-		    "postman-token": "9f4b496b-b219-766a-d2ee-86c2ed7afea8"
+		    "postman-token": "9f4b496b-b219-766a-d2ee-86c2ed7afea8",
+		    "Content-Type": "application/json"
 		  },
 		  "processData": false,
-		  "contentType": false,
+		  "contentType": "application/json",
 		  "mimeType": "multipart/form-data",
-		  "data": form
+		  "data": {
+			"client_id": "518dd73af2d377583d18",
+			"client_secret": "51a19ee8540b5eb840d4ddb4870bcb3a03cc49ac3754aa69800f64299a26",
+			"code": this.apiUrl
+		  }
 		})
     }
 	render() {
